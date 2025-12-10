abe86aa42e9cf2c1da2fda8da20d0524ec9ad371
Strannik37 <Santon@tutamail.com>
2025-12-10 15:57:14 +0300

    poiuytrewq

--- sudoku.py
+++ sudoku.py
@@ -1,152 +1,151 @@
 import random
 import tkinter as tk
 from tkinter import messagebox
+import customtkinter as ctk
 
 
-class SudokuApp(tk.Tk):
+class SudokuOkn(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Судоку Сетка 9x9")
+        self.title("Судоку")
         self.Main()
-        self.sudoku_grid = SudokuGrid(self)
-        self.sudoku_grid.blocks()
-        self.config(bg='turquoise')
+        self.sudoku_setk = SudokuLG(self)
+        self.sudoku_setk.blocks()
+        self.config(bg='#DCDCDC')
          
-        # Кнопка проверки
-        check_button = tk.Button(self, text="проверка",
-                                 command=self.sudoku_otv)
-        check_button.grid(row=9, column=0, columnspan=9, pady=10)
-# расположение
+        check_button = ctk.CTkButton(self, text="ПРОВЕРКА", font=('Arial', 14),
+                                 command=self.sudoku_otv, corner_radius=5, fg_color="#4B4C4C")
+        check_button.grid (row=9 , column=0, columnspan=9 , pady=10)
+
     def Main(self):
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
+        scw = self.winfo_screenwidth()
+        sch = self.winfo_screenheight()
 
-        percent_width = 0.2645
-        percent_height = 0.40
+        pw = 0.423
+        ph = 0.83
 
-        window_width = int(screen_width * percent_width)
-        window_height = int(screen_height * percent_height)
+        ok_w = int(scw * pw)
+        ok_h = int(sch * ph)
 
-        x = (screen_width - window_width) // 2
-        y = (screen_height - window_height) // 2
-        self.geometry(f"{window_width}x{window_height}+{x}+{y}")
+        x = (scw - ok_w) // 2
+        y = (sch - ok_h) // 2
+        self.geometry(f"{ok_w}x{ok_h}+{x}+{y}")
 
     def sudoku_otv(self):
-        all_values = self.sudoku_grid.val()
-        for row in all_values:
+        values = self.sudoku_setk.val()
+        for row in values:
             print(row)
-        result = logic_sudoku(all_values)
+        result = logic_sudoku(values)
         if result == "v":
-            messagebox.showinfo("Проверка", "решено правильно")
+            messagebox.showinfo( "Проверка" , "решено правильно" )
 
         elif result == "0":
-            messagebox.showinfo("Проверка", "таблица не заполнена")
+            messagebox.showinfo( "Проверка", "таблица не заполнена!!!!!!" )
         
         else:
-            messagebox.showinfo("Проверка", "решено с ошибками")
+            messagebox.showinfo( "Проверка", "у тебя ошибки" )
 
-class SudokuGrid:
+class SudokuLG:
     def __init__(self, root):
         self.root = root
-        self.cells = [[None for _ in range(9)] for _ in range(9)]
+        self.cells = [ [None for _ in range(9)] for _ in range(9) ]
 
-# разбиение блоков по цвету 3*3*
     def blocks(self):
         for r in range(9):
             for c in range(9):
-                entry = tk.Entry(self.root, width=3, font=('Arial', 18),
+                entry = tk.Entry(self.root, width=2, font=('Arial', 44) ,
                                  justify='center')
                 entry.grid(row=r, column=c, padx=1, pady=1)
                 self.cells[r][c] = entry
 
-                if (r // 3 + c // 3) % 2 == 0:
-                    entry.config(bg='lightgray')
+
+                if (r/3 + c/3) % 2 == 0 :
+                    entry.config(bg='#A5A5A5')
                 else:
-                    entry.config(bg='lightgray')
+                    entry.config(bg='#A5A5A5')
 
         zadacha = generate_zadacha()
         for r in range(9):
             for c in range(9):
                 if zadacha[r][c] != 0:
-                    self.cells[r][c].insert(0, str(zadacha[r][c]))
-                    self.cells[r][c].config(state='disabled')
+                    self.cells[r][c].insert( 0, str(zadacha[r][c]))
+                    self.cells[r][c].config( state='disabled')
 
 
     def val(self):
         values = []
         for row in self.cells:
-            row_values = []
+            r_val = []
             for entry in row:
                 val = entry.get()
                 if val.isdigit() and 1 <= int(val) <= 9:
-                    row_values.append(int(val))
+                    r_val.append(int(val))
                 else:
-                    row_values.append(0)
-            values.append(row_values)
+                    r_val.append(0)
+            values.append(r_val)
         return values
     
 
-def can_place(grid, row, col, num):
-    if num in grid[row]:
+def can_place(setk, row, col, num):
+    if num in setk[row]:
         return False
-    if num in [grid[r][col] for r in range(9)]: 
+    if num in [setk[r][col] for r in range(9)]: 
         return False
-    sr, sc = 3 * (row // 3), 3 * (col // 3)
-    for r in range(sr, sr + 3):
-        for c in range(sc, sc + 3):
-            if grid[r][c] == num: 
+    s_rw, s_cl = 3 * ( row//3), 3 * ( col//3)
+    for r in range( s_rw, s_rw + 3):
+        for c in range( s_cl, s_cl + 3):
+            if setk[r][c] == num: 
                 return False
     return True
     
-def resh(grid):
+def resh(setk):
     for r in  range(9):
-        for c in  range(9):
-            if grid[r][c] == 0:
-                nums = list(range(1, 10))
-                random.shuffle(nums)
-                for num in nums:
-                    if can_place(grid, r, c, num):
-                        grid[r][c] = num
-                        if resh(grid): 
+        for c in range(9):
+            if setk[r][c] == 0:
+                ns = list(range(1, 10))
+                random.shuffle(ns)
+                for num in ns:
+                    if can_place(setk, r, c, num):
+                        setk[r][c] = num
+                        if resh(setk): 
                             return True
-                        grid[r][c] = 0
+                        setk[r][c] = 0
                 return False
     return True
 
-def blk(grid):
+def blk(setk):
     for k in range (0, 9, 3):
-        nums = list (range(1, 10))
-        random.shuffle(nums)
+        ns = list (range(1, 10))
+        random.shuffle(ns)
         for i in range(3):
             for j in range(3):
-                grid[k+i][k+j] = nums.pop()
+                setk[k+i][k+j] = ns.pop()
             
-
 def gener():
-    grid = [[0] * 9 for _ in range(9)]
-    blk(grid)
-    resh(grid)
-    return grid
+    setk = [[0] * 9 for _ in range(9)]
+    blk(setk)
+    resh(setk)
+    return setk
 
 def generate_zadacha():
-    grid = gener()
-    cells_to_remove = random.sample(range(81), 61)
-    for idx in cells_to_remove:
+    setk = gener()
+    kletk_del = random.sample(range(81), 61)
+    for idx in kletk_del:
         r, c = divmod(idx, 9)
-        grid[r][c] = 0
-    return grid
+        setk[r][c] = 0
+    return setk
 
                 
-def logic_sudoku(grid):
+def logic_sudoku(setk):
 
-    for row in grid:
+    for row in setk:
         num = [n for n in row if n != 0]
         if len(num) != len(set(num)):
             print("ошибка в строке")
             return "x"
     
     for c in range(9):
-        col = [grid[r][c] for r in range(9) if grid[r][c] != 0]
+        col = [setk[r][c] for r in range(9) if setk[r][c] != 0]
         if len(col) != len(set(col)):
             print("ошибка в колонке")
             return "x"
@@ -156,13 +155,13 @@ def logic_sudoku(grid):
             block = []
             for r in range (br, br+3):
                 for c in range (bc, bc+3):
-                    if grid[r][c] != 0 :
-                        block.append(grid[r][c])
+                    if setk[r][c] != 0 :
+                        block.append(setk[r][c])
             if len(block) != len(set(block)):
                 print('ошибка в блоке 3*3')
                 return "x"
     
-    for  row  in grid:
+    for  row  in setk:
         if 0 in row:
             print('сетка не заполнена ')
             return "0"
@@ -170,5 +169,5 @@ def logic_sudoku(grid):
     return "v"
 
 if __name__ == "__main__":
-    app = SudokuApp()
+    app = SudokuOkn()
     app.mainloop()
